8e520bf976cff2c2cb882ac814f7ab87a16b02fe
Pedro Faleiros <pedro.alves.faleiros@gmail.com>
2025-05-17 17:26:09 -0300

    adicionando o carrossel no home

---
+++ src/js/script.js
@@ -0,0 +1,28 @@
+let imagens = [
+  "src/assets/imagem1.jpg",
+  "src/assets/imagem2.jpg",
+  "src/assets/imagem3.jpg",
+  "src/assets/imagem4.jpg",
+  "src/assets/imagem5.jpg",
+  "src/assets/imagem6.jpg",
+  "src/assets/imagem7.jpg",
+  "src/assets/imagem8.jpg",
+  "src/assets/imagem9.jpg",
+  "src/assets/imagem10.jpg",
+  "src/assets/imagem11.jpg",
+];
+
+let i = 0;
+
+function slideShow() {
+  document.getElementById("image").src = imagens[i];
+  i++;
+  console.log(i);
+  if (i == imagens.length) {
+    i = 0;
+  }
+
+  setTimeout("slideShow()", 3000);
+}
+
+slideShow();
